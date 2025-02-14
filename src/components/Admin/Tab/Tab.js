1ac2352db26123299773cb272791871cda98bbcd
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-02-14 15:15:24 +0700

    push

--- src/components/Admin/Tab/Tab.js
+++ src/components/Admin/Tab/Tab.js
@@ -20,49 +20,14 @@ const Tab = () => {
         <div className="tab-icon">
           <i className="fa-solid fa-book"></i>
         </div>
-        <div className="tab-link">Khoá học</div>
-      </NavLink>
-
-      <NavLink className="tab-item" to="/admin/try">
-        <div className="tab-icon">
-          <i className="fa-solid fa-pen-nib"></i>
-        </div>
-        <div className="tab-link">Luyện tập</div>
-      </NavLink>
-
-      <NavLink className="tab-item" to="/admin/forum">
-        <div className="tab-icon">
-          <i className="fa-solid fa-layer-group"></i>
-        </div>
-        <div className="tab-link">Diễn đàn</div>
+        <div className="tab-link">Sản phẩm</div>
       </NavLink>
 
       <NavLink className="tab-item" to="/admin/blog">
         <div className="tab-icon">
           <i className="fa-solid fa-blog"></i>
         </div>
-        <div className="tab-link">Blog</div>
-      </NavLink>
-
-      <NavLink className="tab-item" to="/admin/events">
-        <div className="tab-icon">
-          <i className="fa-solid fa-calendar-days"></i>
-        </div>
-        <div className="tab-link">Sự kiện</div>
-      </NavLink>
-
-      <NavLink className="tab-item" to="/admin/news">
-        <div className="tab-icon">
-          <i className="fa-solid fa-newspaper"></i>
-        </div>
-        <div className="tab-link">Tin tức</div>
-      </NavLink>
-
-      <NavLink className="tab-item" to="/admin/themes">
-        <div className="tab-icon">
-          <i className="fa-solid fa-palette"></i>
-        </div>
-        <div className="tab-link">Giao diện</div>
+        <div className="tab-link">Bài viết</div>
       </NavLink>
 
       <NavLink className="tab-item" to="/admin/bins">
